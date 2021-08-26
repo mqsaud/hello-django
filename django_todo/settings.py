470d3ee328663e58d9915cc32f42a03316649a5a
mqsaud <mqsaud@gmail.com>
2021-08-26 22:22:45 +0000

    Prepare to depoly to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,11 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://bnbibwvcvutdkw:08c74de075edb408b9b5de434f44fabe4bc7fcdedf8026686592eee2412fdf09@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/d40e7g0snnurak')
 }
 
 
